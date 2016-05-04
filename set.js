65c4742d7d75291e3442871cf717d8d60df40271
zhengshizhao <zhao.zhengshi@gmail.com>
2016-05-04 15:00:54 -0400

    uddated solution to handle an arbitrary size for the dimensions

--- set.js
+++ set.js
@@ -4,18 +4,26 @@
 
 // Task 3: Then, once you've got arbitrary dimensions, extend your solution to handle an arbitrary size for the dimensions (e.g. there are now four possible shapes, four colors, four of every dimension). You may assume the number of cards remains three, or solve for an arbitrary number greater than three for extra credit.
 
-// In your response, please send (or link to github, etc):
-
 var dimentions = ["color","number","shape","shading"];
-var dimentionNum = 3;
+var dimentionNum = 4; 
 var cardNum = 3;
-var cardsdeck = 10;
-var dimention = [["1","2","3"],["1","2","3"],["1","2","3"],["1","2","3"]]
-var colors = ["1","2","3"];
-var numbers = ["1","2","3"];
-var shapes = ["1","2","3"];
-var shadings = ["1","2","3"];
+var cardsdeck = 15;
+var dimentionValues = {
+	color: [],
+	number:[],
+	shape:[],
+	shading:[]
+};
+var dimention = [];
 
+for (var key in dimentionValues) {
+	if (dimentionValues.hasOwnProperty(key)) {
+	for(var j =1; j<= dimentionNum; j++) {
+		dimentionValues[key].push(j);
+	}
+	dimention.push(dimentionValues[key]);
+	}
+}
 function Card(color,number,shape,shading) {
 	this.color = color;
 	this.number = number;
@@ -25,49 +33,47 @@ function Card(color,number,shape,shading) {
 function collection (cardsNumber){
 	var cards = [];
 	for (var i=0; i<cardsNumber; i++){
-		cards.push(new Card(colors[getRandomArbitrary()], numbers[getRandomArbitrary()], shapes[getRandomArbitrary()],shadings[getRandomArbitrary()]));
+		cards.push(new Card(dimentionValues.color[getRandomArbitrary()], dimentionValues.number[getRandomArbitrary()], dimentionValues.shape[getRandomArbitrary()],dimentionValues.shading[getRandomArbitrary()]));
 	}
 	return cards;
 }
-
 function getRandomArbitrary() {
   return Math.floor(Math.random() * dimentionNum);
 }
-function allsameOrAlldifferent(dimentionArr, dementionString){
-	if(dimentionArr.join("") === dementionString) return "different";
-	else if (dimentionArr.every(function(ele) {return ele === dimentionArr[0];})) return "same";
+function allsameOrAlldifferent(dimentionArr){
+	function isDifferent(dimentionArr) {
+		if (dimentionArr.length === 0) return true;
+		if (dimentionArr.slice(1).some(function(ele){return ele === dimentionArr[0];})) return false;
+		else {return isDifferent(dimentionArr.slice(1));}
+	}
+	function isSame(dimentionArr){
+		if (dimentionArr.every(function(ele) {return ele === dimentionArr[0];})) return true;
+	}
+	
+	if(isDifferent(dimentionArr)) return "different";
+	else if (isSame(dimentionArr)) return "same";
 	else return;
 }
-function isSet(threeCards) {
-	//console.log("threeCards",threeCards);
+function isSet(cardsToCheck) {
 	var result = [];
 	var dementionSet = [];
 	function getdimention (key){
-		return threeCards.map(
+		return cardsToCheck.map(
 			function(card){ 
 				return card[key];});
 	}
 	
 	for (var i = 0; i < dimentions.length; i++){
 		dementionSet[i] = getdimention(dimentions[i]);
-	    var compareResult = allsameOrAlldifferent(dementionSet[i].sort(), dimention[i].join(""));
+	    var compareResult = allsameOrAlldifferent(dementionSet[i].sort());
 	    if(!compareResult) return false; 
 	    else {result[i] = compareResult;
-	    //console.log("dementionSet:", dementionSet,"result:", result);	
 	    }
 	}
 	result = result.sort();
-	// console.log("result,", result.slice(0,1),result.slice(1).join(""));
-	// console.log("result,", result.slice(0,dimentions.length-1).join(""), result.slice(dimentions.length-1));
-	
-	// console.log((result.slice(0,1).toString() === 'different' && allsameOrAlldifferent(result.slice(1),"samesamesame")) )
-	// console.log((allsameOrAlldifferent(result.slice(0,dimentions.length),"differentdifferentdifferent") && (result.slice(dimentions.length-1).toString() === "same")))
-	if((result.slice(0,1).toString() === 'different' && allsameOrAlldifferent(result.slice(1),"samesamesame")) || (allsameOrAlldifferent(result.slice(0,dimentions.length-1),"differentdifferentdifferent") && (result.slice(dimentions.length-1).toString() === "same"))) {
-		//console.log("this is a set!")
+	if((result.slice(0,1).toString() === 'different' && result.slice(1).join("")==="samesamesame") || (result.slice(0,dimentions.length-1).join("") === "differentdifferentdifferent" && (result.slice(dimentions.length-1).toString() === "same"))) {
 		return true;
-		
 	}
-
 }
 function setCollection(cards) {
 	var set = {};
@@ -84,14 +90,23 @@ function setCollection(cards) {
 			}
 		}
 	}
-	if (set.length === 0){return "no combination to be a set!"}
+	if (set.length === 0){ console.log("no set cobmination");}
 	else { return set;}
 }
 var cards1 = [{color: '3', number: '2', shape: '1', shading: '2'},{color: '3', number: '2', shape: '1', shading: '3'},{color: '3', number: '2', shape: '1', shading: '1'},{color: '3', number: '1', shape: '2', shading: '1'},{color: '3', number: '3', shape: '3', shading: '3'}];
 var cards2 = collection(cardsdeck);
-console.log("input cards:")
+
+
+console.log("input cards1:");
 console.log(cards1);
 console.log("set", setCollection(cards1));
-console.log("input cards,");
+console.log("input cards2,");
 console.log(cards2);
 console.log("set", setCollection(cards2));
+
+//un-comment below when testing the case of dimentionNum = 4;
+// var cards3 = [{color: '3', number: '2', shape: '1', shading: '2'},{color: '3', number: '2', shape: '1', shading: '3'},{color: '3', number: '2', shape: '1', shading: '1'},{color: '3', number: '1', shape: '2', shading: '1'},{color: '3', number: '3', shape: '3', shading: '3'},{color: '3', number: '2', shape: '1', shading: '4'}];
+// console.log("input cards3,");
+// console.log(cards3);
+// console.log("set", setCollection(cards3));
+
